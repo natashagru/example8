f5783b07d57b84539705e92254b9f033f06c67d6
natalia <natka545@mail.ru>
2022-10-04 20:21:44 +0300

    Решили задачу 8

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// программа, которая принимает на вход принимает число N, а на выходе показывает все четные числа от 1 до N
+
+Console.Clear();
+Console.WriteLine("введите число n");
+int n = int.Parse(Console.ReadLine()!);
+int n2 = 2;
+while (n2 <= n)
+{
+    Console.WriteLine($"{n2}");
+    n2 = n2 + 2;
+}
